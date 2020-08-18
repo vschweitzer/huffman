c3ae810a764dae394d35faeabd4b3ca89d4c81d4
vschweitzer <valentin.schweitzer98@gmail.com>
2020-08-18 17:43:57 +0200

    Start support of more than two codes in compress function

--- compress.c
+++ compress.c
@@ -21,7 +21,7 @@ uint64_t countCodes(char * filename, codeWord ** code_array) {
 
     while(!feof(input_file)) {
         uint8_t byte;
-        
+
         value = fgetc(input_file);
         if(feof(input_file)) {
             break;
@@ -30,13 +30,13 @@ uint64_t countCodes(char * filename, codeWord ** code_array) {
         byte = (uint8_t) value;
         ++(code_array[byte]->count);
     }
-    
+
     total_count = ftell(input_file);
     fclose(input_file);
     return total_count;
 }
 
-// This function is overly complicated, because it has to account for 
+// This function is overly complicated, because it has to account for
 // differences in counts that do not fit in an int
 int compareCodeCount(const void * a, const void * b) {
     if((*(codeWord **)a)->count >= (*(codeWord **)b)->count) {
@@ -56,8 +56,8 @@ void printCodes(codeWord ** code_array) {
 
     for(unsigned int i = 0; i < 256; ++i) {
         if(code_array[i]->count) {
-            printf("Byte:  %#04x\nChar:  %c\nCount: %"PRIu64"\n", 
-                code_array[i]->byte, 
+            printf("Byte:  %#04x\nChar:  %c\nCount: %"PRIu64"\n",
+                code_array[i]->byte,
                 isprint(code_array[i]->byte) ? code_array[i]->byte : ' ',
                 code_array[i]->count
                 );
@@ -87,18 +87,26 @@ codeTree * makeTree(codeWord ** code_array, uint8_t length_minus_one) {
 
         one->code = code_array[1];
         one->subcount = one->code->count;
-        
+
         root->zero = zero;
         root->one = one;
         root->subcount = zero->subcount + one->subcount;
 
         return root;
     } else {
+        // First two codeWords build the first tree
+        uint8_t index = 2;
         uint8_t counter_minus_one = length_minus_one;
-        {
+        codeTree * root;
+        // Make first tree out of first two codeWords
+        root = makeTree(code_array, 1);
+
+        do {
+
+
             printf("Counter: %d\n", counter_minus_one);
         } while (counter_minus_one--);
-        
+
         return NULL;
     }
 }
